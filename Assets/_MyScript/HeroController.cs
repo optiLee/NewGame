36c71c92ae34a226b4ad4866a978d050de91ec3a
optiLee <mypanics@gmail.com>
2015-07-30 23:49:21 +0900

    first
    first commit

---
+++ Assets/_MyScript/HeroController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroController : MonoBehaviour
+{
+	float speed = 100f;
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		float dirX = Input.GetAxis("Horizontal");
+		//if (dirX != 0)
+		//	print ("Update : " +dirX);
+
+		Vector2 movement = new Vector2 (dirX, 0);		
+		GetComponent<Rigidbody2D>().velocity = movement * speed;
+		//GetComponent<Rigidbody2D> ().rotation = Quaternion.Euler (0, 0, GetComponent<Rigidbody>().velocity.x * -tilt);
+
+		// 좌우 이동에 따른 Hero 방향변경
+		if (dirX < 0)
+			GetComponent<RectTransform>().localScale = new Vector3(-1, 1, 1);
+		else if (dirX > 0)
+			GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+	}
+}
---
+++ Assets/_MyScript/MonsterController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterController : MonoBehaviour {
+
+	float speed = 200f;
+
+	// Use this for initialization
+	void Start () {
+		bool isSpawn = false;
+
+		if (!isSpawn) {
+			//GetComponent<RectTransform> ().position.x = Random.Range (-320, 650);
+			//GetComponent<RectTransform> ().position.y = -175;
+			//GetComponent<RectTransform> ().localScale = new Vector2 (20, -75);
+			GetComponent<RectTransform> ().localPosition = new Vector2 (Random.Range (-500, 200), -175);
+
+			float dirY = Input.GetAxis ("Vertical");
+
+			Vector2 movement = new Vector2 (dirY, 0);	
+			GetComponent<Rigidbody2D> ().velocity = movement * speed;
+
+			if(GetComponent<RectTransform> ().localPosition.y > -120)
+				isSpawn = true;
+		}
+
+		float dirX = Input.GetAxis("Horizontal");
+		Vector2 movementX = new Vector2 (dirX, 0);
+		GetComponent<Rigidbody2D> ().velocity = movementX * speed;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
